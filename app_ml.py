70812180325a561d670b212abdcd7bb0108a53c0
jung hyun <hfghhghg11@gmil.com>
2022-05-24 09:58:09 +0900

    app

--- app_ml.py
+++ app_ml.py
@@ -1,43 +1,40 @@
+from codecs import getencoder
+from pytest import importorskip
 import streamlit as st
-import pandas as pd
-import os
 import numpy as np
-import joblib
+
 import sklearn
 
-def run_ml():
+import joblib
+
+def run_ml() :
     st.subheader('자동차 구매 가능 금액 예측')
 
     print(sklearn.__version__)
 
-    # 예측하기 위해서 필요한 파일들을 불러와야 된다
-    # 이 예에서는 ,인공지능파일 ,X 스케일러 파일 ,y 스케일러파일
+    # 예측하기 위해서 필요한 파일들을 불러와야 된다.
+    # 이 예에서는, 인공지능파일, X 스케일러 파일, y 스케일러파일
     # 3개를 불러와야 한다.
 
     regressor = joblib.load('data/regressor.pkl')
     scaler_X = joblib.load('data/scaler_X.pkl')
-    scaler_y = joblib.load('data/scaler_y.pkl')
+    scaler_y =joblib.load('data/scaler_y.pkl')
 
-
-    # 성별, 나이, 연봉, 카드빛, 자산 을 입력받도록 만드세요.
-    
-    gender = st.radio('성별 입력',['남자','여자'])
-
+    # 성별, 나이, 연봉, 카드빚, 자산을 입력받고록 만드세요.
+    gender = st.radio('성별을 입력하세요.',['남자','여자'])
     if gender == '여자' :
         gender = 0
     else :
         gender = 1
+    age = st.number_input('나이 입력',0,120)
+    Salary = st.number_input('연봉 입력',0)
+    debt = st.number_input('카드빛 입력',0)
+    worth = st.number_input('자산 입력',0)
 
-    age = st.number_input('나이 입력', 0, 120)
-    salary = st.number_input('연봉 입력', 0)
-    debt = st.number_input('카드빚 입력', 0)
-    worth = st.number_input('자산 입력', 0)
-
-    if st.button('자동차 구매 금액 예측') :
-
+    if st.button('자동차 구매 금액 예측') : 
 
         # 1. 신규 고객의 정보를 넘파이 어레이로 만들어준다.
-        new_date = np.array([gender,age,salary,debt,worth])
+        new_data = np.array([gender,age,Salary,debt,worth])
 
         # 2. 학습할때 사용한 X 의 피처 스케일러를 이용해서, 피처스케일링하기
         # 먼저, 데이터를 2차원으로 만들어준다.
@@ -51,5 +48,4 @@ def run_ml():
         y_pred = scaler_y.inverse_transform(y_pred)
 
         y_pred = round(y_pred[0,0])
-        
-        st.write('이 사람의 구매 가능 금액은' + str(y_pred) + '달러입니다.')
+        st.write('이 사람의 구매 가능 금액은'  + str(y_pred) + '달러입니다.')
